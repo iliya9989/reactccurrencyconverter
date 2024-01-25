38314ec197b89b602d67630930f32626e1cdd4af
iliya9989 <iliya9989@gmail.com>
2024-01-25 08:15:19 +0100

    rebuild

--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -1,18 +1,44 @@
 import React from "react";
+import { Row, Col, Form } from "react-bootstrap";
 
 function CurrencyRow(props) {
-  const { currencyOptions, selectedCurrency, onChangeCurrency, amount, onChangeAmount } = props;
+  const {
+    currencyOptions,
+    selectedCurrency,
+    onChangeCurrency,
+    amount,
+    onChangeAmount,
+  } = props;
+
   return (
-    <div>
-      <input type="number" className="input" value={amount} onChange={onChangeAmount}/>
-      <select value={selectedCurrency} onChange={onChangeCurrency}>
-        {currencyOptions.map((option) => (
-          <option key={option} value={option}>
-            {option}
-          </option>
-        ))}
-      </select>
-    </div>
+    <Row className="currency-row">
+      <Col xs={6} className="text-center">
+        <Form.Group controlId="currency">
+          <Form.Label>Select Currency</Form.Label>
+          <Form.Control
+            as="select"
+            value={selectedCurrency}
+            onChange={onChangeCurrency}
+          >
+            {currencyOptions.map((currency) => (
+              <option key={currency} value={currency}>
+                {currency}
+              </option>
+            ))}
+          </Form.Control>
+        </Form.Group>
+      </Col>
+      <Col xs={6} className="text-center">
+        <Form.Group controlId="amount">
+          <Form.Label>Amount</Form.Label>
+          <Form.Control
+            type="number"
+            value={isNaN(amount) ? "" : amount}
+            onChange={onChangeAmount}
+          />
+        </Form.Group>
+      </Col>
+    </Row>
   );
 }
 
